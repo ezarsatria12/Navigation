329f647f243f120a428b219647701e118a556820
Ezarsatria12 <Ezar3004>
2020-11-27 14:06:47 +0700

    scholl assignment

---
+++ app/src/main/java/com/ezar/mynavigation/DetailCategoryFragment.kt
@@ -0,0 +1,46 @@
+package com.nunu.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_detail_category.*
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailCategoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailCategoryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
+        tv_category_name.setText(dataName)
+        tv_category_description.setText("Stock : $dataDescription")
+        btn_profile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
+
+    }
+}
